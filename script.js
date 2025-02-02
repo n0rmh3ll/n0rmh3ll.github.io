ec9ea747bffafb21a90e3ae70335ff43765a5852
n0rmh3ll <89399773+n0rmh3ll@users.noreply.github.com>
2025-02-02 18:01:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("resumeModal")
   const btn = document.getElementById("resumeBtn")
   const span = document.getElementsByClassName("close")[0]
-  const viewBtn = document.getElementById("viewResumeBtn")
-  const downloadBtn = document.getElementById("downloadResumeBtn")
-
-  // Replace this with your actual Google Drive file ID
-  const fileId = "1_qa6W2q_3wImiBqLfT3URBoetGx1xFjK"
-  const viewUrl = `https://drive.google.com/file/d/${fileId}/view`
-  const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`
 
   btn.onclick = () => {
     modal.style.display = "block"
@@ -23,18 +16,5 @@ document.addEventListener("DOMContentLoaded", () => {
       modal.style.display = "none"
     }
   }
-
-  viewBtn.onclick = (e) => {
-    e.preventDefault()
-    window.open(viewUrl, "_blank")
-  }
-
-  downloadBtn.onclick = (e) => {
-    e.preventDefault()
-    window.open(downloadUrl, "_blank")
-  }
-
-  viewBtn.href = viewUrl
-  downloadBtn.href = downloadUrl
 })
 
