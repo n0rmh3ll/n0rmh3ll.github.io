c397bc19fba521b38cc870ed664de34f71c3c5da
n0rmh3ll <89399773+n0rmh3ll@users.noreply.github.com>
2025-02-02 17:43:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var modal = document.getElementById("resumeModal");
     var btn = document.getElementById("resumeBtn");
     var span = document.getElementsByClassName("close")[0];
+    var viewBtn = document.getElementById("viewResumeBtn");
+    var downloadBtn = document.getElementById("downloadResumeBtn");
+    var resumePreview = document.getElementById("resumePreview");
+    var resumeFrame = document.getElementById("resumeFrame");
+
+    var pdfPath = "/content/resume.pdf";
 
     btn.onclick = function() {
         modal.style.display = "block";
@@ -9,11 +15,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     span.onclick = function() {
         modal.style.display = "none";
+        resumePreview.style.display = "none";
     }
 
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            resumePreview.style.display = "none";
         }
+    }
+
+    viewBtn.onclick = function() {
+        if (isMobile()) {
+            window.open(pdfPath, '_blank');
+        } else {
+            resumePreview.style.display = "block";
+            resumeFrame.src = pdfPath;
+        }
+    }
+
+    downloadBtn.onclick = function(event) {
+        if (isMobile()) {
+            event.preventDefault();
+            window.open(pdfPath, '_blank');
+        }
+    }
+
+    function isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     }
 });
